73b7662b1f06b8fda806660eeaff349ef92f4f7a
Aleksandr Sokolov <algensok@gmail.com>
2024-04-04 01:13:59 +0300

    2.2 databases-2 is completed

--- 2.2-databases-2/m2m-relations/articles/admin.py
+++ 2.2-databases-2/m2m-relations/articles/admin.py
@@ -1,8 +1,38 @@
 from django.contrib import admin
 
-from .models import Article
+from django.core.exceptions import ValidationError
+
+from django.forms import BaseInlineFormSet
+
+from .models import Article, Tag, Scope
+
+
+class RelationshipInlineFormSet(BaseInlineFormSet):
+    def clean(self):
+        i = 0
+        for form in self.forms:
+            dictionary = form.cleaned_data
+            if dictionary.get('is_main'):
+                i += 1
+            else:
+                continue
+        if i == 0:
+            raise ValidationError('Укажите основной раздел')
+        elif i > 1:
+            raise ValidationError('Основным может быть только один раздел')
+        return super().clean()
+
+
+class TagsTitleInline(admin.TabularInline):
+    model = Scope
+    formset = RelationshipInlineFormSet
 
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
+    inlines = [TagsTitleInline,]
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
     pass
