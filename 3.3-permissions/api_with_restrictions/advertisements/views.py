1218eba54b76f74fca5a209a401f5cfc5ade999a
Aleksandr Sokolov <algensok@gmail.com>
2024-04-14 03:20:25 +0300

    3.3 permission is completed

--- 3.3-permissions/api_with_restrictions/advertisements/views.py
+++ 3.3-permissions/api_with_restrictions/advertisements/views.py
@@ -1,5 +1,15 @@
-from rest_framework.permissions import IsAuthenticated
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter, OrderingFilter
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 from rest_framework.viewsets import ModelViewSet
+from .models import Advertisement
+from .serializers import AdvertisementSerializer
+from .permissions import IsOwnerOrReadOnly
+from .filters import AdvertisementFilter
+
+
+class CustomSearchFilter(SearchFilter):
+    search_param = 'creator'
 
 
 class AdvertisementViewSet(ModelViewSet):
@@ -7,6 +17,16 @@ class AdvertisementViewSet(ModelViewSet):
 
     # TODO: настройте ViewSet, укажите атрибуты для кверисета,
     #   сериализаторов и фильтров
+    queryset = Advertisement.objects.all()
+    serializer_class = AdvertisementSerializer
+    permission_classes = [IsOwnerOrReadOnly]
+    filter_backends = [DjangoFilterBackend, OrderingFilter, CustomSearchFilter]
+    search_fields = ['title', 'description', 'creator__id', 'status']
+    ordering_fields = ['id', 'user', 'created_at']
+    filterset_class = AdvertisementFilter
+
+    def perform_create(self, serializer):
+        serializer.save(creator=self.request.user)
 
     def get_permissions(self):
         """Получение прав для действий."""
